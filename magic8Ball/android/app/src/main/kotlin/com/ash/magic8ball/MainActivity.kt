22a5678c320c3af04ff704ad6aace193f17fa1a8
unknown <mohammedashraf083@gmail.com>
2023-11-24 12:57:53 +0600

    Magic 8 ball complete

---
+++ magic8Ball/android/app/src/main/kotlin/com/ash/magic8ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ash.magic8ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ magic8Ball/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan.shade900,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Magic 8 ball'),
+          ),
+          backgroundColor: Colors.cyan.shade800,
+        ),
+        body: MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({super.key});
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
