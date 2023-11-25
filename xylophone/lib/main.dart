7e1359cd54c76d5e050deb6435ad000186a130f0
unknown <mohammedashraf083@gmail.com>
2023-11-25 18:29:03 +0600

    UI updated

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -20,50 +20,64 @@ class _XylophoneState extends State<Xylophone> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                style: ElevatedButton.styleFrom(primary: Colors.redAccent),
-                onPressed: () {
-                  playSound(1);
-                },
-                child: Text(''),
+              Expanded(
+                child: TextButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.redAccent),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Text(''),
+                ),
               ),
-              TextButton(
-                style: ElevatedButton.styleFrom(primary: Colors.greenAccent),
-                onPressed: () {
-                  playSound(2);
-                },
-                child: Text(''),
+              Expanded(
+                child: TextButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.greenAccent),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Text(''),
+                ),
               ),
-              TextButton(
-                style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
-                onPressed: () {
-                  playSound(3);
-                },
-                child: Text(''),
+              Expanded(
+                child: TextButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Text(''),
+                ),
               ),
-              TextButton(
-                style: ElevatedButton.styleFrom(primary: Colors.orangeAccent),
-                onPressed: () {
-                  playSound(4);
-                },
-                child: Text(''),
+              Expanded(
+                child: TextButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.orangeAccent),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Text(''),
+                ),
               ),
-              TextButton(
-                style: ElevatedButton.styleFrom(primary: Colors.black26),
-                onPressed: () {
-                  playSound(5);
-                },
-                child: Text(''),
+              Expanded(
+                child: TextButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.pink),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Text(''),
+                ),
               ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.purpleAccent),
-                onPressed: () {
-                  playSound(6);
-                },
-                child: Text(''),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.purpleAccent),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Text(''),
+                ),
               ),
             ],
           ),
